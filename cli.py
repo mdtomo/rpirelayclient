be88b1793f66bf18b7722b93530a7bf21495f54c
Matthew Thomas <mdthomas@hotmail.ca>
2018-10-29 18:29:36 -0700

    Created basic key bindings.

--- cli.py
+++ cli.py
@@ -4,28 +4,42 @@ import prompt_toolkit as ptk
 from prompt_toolkit import widgets
 from prompt_toolkit.eventloop import use_asyncio_event_loop
 from prompt_toolkit.buffer import Buffer
-from prompt_toolkit.layout.containers import VSplit, Window
+from prompt_toolkit.layout.containers import VSplit, HSplit, Window
 from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
 from prompt_toolkit.layout.layout import Layout
 from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.styles import Style
+from prompt_toolkit.widgets import MenuItem
 from datetime import datetime
+from config import Config as config
 
 
 HOST = '127.0.0.1'
 PORT = 2018
 
 kb = KeyBindings()
-buffer1 = Buffer()
+channel_status = [1,1,0,0,0,0,0,0]
+channel_status_labels = [FormattedTextControl('Enabled', style=u'#5faf00') if status == 1 else FormattedTextControl('Disabled', style=u'#ff5f5f') for status in channel_status]
+log_title = FormattedTextControl('test')
 root_container = widgets.MenuContainer(
     VSplit([
-        Window(content=BufferControl(buffer=buffer1)),
         widgets.Shadow(
             widgets.Frame(
-            Window(content=FormattedTextControl(text='System Logs')),
-            title='System logs')
+                #HSplit([Window(height=2, char=' ')]),
+                VSplit([
+                    HSplit([widgets.Label('Channel ' + str(i)) for i in range(1,9)]),
+                    HSplit([widgets.Label(label, style=u'#ffaf00') for label in config.RELAY_LABELS]),
+                    HSplit([Window(status) for status in channel_status_labels]),
+                ]),
+                title='Relay Status')
+        ),
+        widgets.Shadow(
+            widgets.Frame(
+                Window(content=log_title),
+                title='Test')
         )
     ]),
-    menu_items=[widgets.menus.MenuItem(text='Menu')],
+    menu_items=[MenuItem('Status', children=[MenuItem('Update')])],
 )
 layout = Layout(root_container)
 
@@ -35,7 +49,8 @@ use_asyncio_event_loop()
 
 def main():
     app = ptk.Application(layout=layout, key_bindings=kb, full_screen=True)
-    app.run()
+    #app.run()
+    asyncio.get_event_loop().run_until_complete(app.run_async().to_asyncio_future())
     asyncio.run(create_connection())
 
 
@@ -54,8 +69,17 @@ async def send_data(data):
 
 
 @kb.add('q')
-def exit(event):
+def quit(event):
     event.app.exit()
+
+
+@kb.add('1')
+def _(event):
+    # event.app.reset()
+    global channel_status_labels
+    channel_status_labels[0].text = 'test'
+    global log_title
+    log_title.text = 'Logs 123'
 
 
 if __name__ == '__main__':
---
+++ config.py
@@ -0,0 +1,5 @@
+
+class Config:
+
+    RELAY_LABELS = ['Exterior Stairs Light', 'Deck Pot Lights', 'Soffit Receptacle', 'Driveway Light']
+    
