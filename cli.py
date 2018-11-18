1ecdc3f7e690a22141f982aa08f507b61e1ff93f
Matthew Thomas <mdthomas@hotmail.ca>
2018-11-17 23:32:38 -0800

    Added custom Button class.

--- cli.py
+++ cli.py
@@ -1,86 +1,170 @@
 import socket
 import asyncio
 import prompt_toolkit as ptk
+from prompt_toolkit.application import get_app
 from prompt_toolkit import widgets
 from prompt_toolkit.eventloop import use_asyncio_event_loop
 from prompt_toolkit.buffer import Buffer
 from prompt_toolkit.layout.containers import VSplit, HSplit, Window
 from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
 from prompt_toolkit.layout.layout import Layout
+from prompt_toolkit.layout import Float, FloatContainer
 from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.styles import Style
-from prompt_toolkit.widgets import MenuItem
+from prompt_toolkit.widgets import MenuItem, Button
 from datetime import datetime
 from config import Config as config
+from controls import ChannelSwitch
 
 
 HOST = '127.0.0.1'
 PORT = 2018
+program_quitting = False
+style_warning = '#800000 bg:#a8a8a8 bold reverse'
+style_ok = '#005f00 bg:#a8a8a8 bold'
+use_asyncio_event_loop()
+global_kb = KeyBindings()
+channel_kb = KeyBindings()
+channel_status = [1,0,1,0,0,0,0,0]
+info_bar = FormattedTextControl('Press TAB to toggle channels.', style='#0087d7')
 
-kb = KeyBindings()
-channel_status = [1,1,0,0,0,0,0,0]
-channel_status_labels = [FormattedTextControl('Enabled', style=u'#5faf00') if status == 1 else FormattedTextControl('Disabled', style=u'#ff5f5f') for status in channel_status]
+def toggle_channel(number):
+    current_window = get_app().layout.current_window
+    parent = get_app().layout.get_parent(current_window)
+    info_bar.text = str(ChannelSwitch.instances[number].status)
+    if isinstance(current_window, ChannelSwitch):
+        current_control.status = not current_control.status
+        info_bar.text = 'changed'
+
+
+#channel_status_labels = [FormattedTextControl('Enabled', style=u'#5faf00', focusable=True, show_cursor=True, key_bindings=channel_kb) if status == 1 else FormattedTextControl('Disabled', style=u'#ff5f5f', focusable=True, show_cursor=True, key_bindings=channel_kb) for status in channel_status]
+channel_status_labels = [ChannelSwitch(True, handler=toggle_channel, handler_args=i) if status == 1 else  ChannelSwitch(False, handler=toggle_channel, handler_args=i) for i, status in enumerate(channel_status)]
 log_title = FormattedTextControl('test')
-root_container = widgets.MenuContainer(
-    VSplit([
-        widgets.Shadow(
-            widgets.Frame(
-                #HSplit([Window(height=2, char=' ')]),
-                VSplit([
-                    HSplit([widgets.Label('Channel ' + str(i)) for i in range(1,9)]),
-                    HSplit([widgets.Label(label, style=u'#ffaf00') for label in config.RELAY_LABELS]),
-                    HSplit([Window(status) for status in channel_status_labels]),
-                ]),
-                title='Relay Status')
-        ),
-        widgets.Shadow(
-            widgets.Frame(
-                Window(content=log_title),
-                title='Test')
-        )
-    ]),
-    menu_items=[MenuItem('Status', children=[MenuItem('Update')])],
-)
+#log_buffer = Buffer('buffer control', multiline=True)
+status_bar = FormattedTextControl('text', style=style_ok)
+
+root_container = HSplit([
+    widgets.MenuContainer(
+        VSplit([
+            widgets.Shadow(
+                widgets.Frame(
+                    VSplit([
+                        HSplit([widgets.Label('Channel ' + str(i)) for i in range(1,9)]),
+                        HSplit([widgets.Label(label, style=u'#ffaf00') for label in config.RELAY_LABELS]),
+                        HSplit([status for status in channel_status_labels]),
+                    ]),
+                    title='Relay Status')
+            ),
+            widgets.Shadow(
+                widgets.Frame(
+                    Window(content=log_title),
+                    title='Test')
+            )
+        ]),
+        menu_items=[MenuItem('Status'), MenuItem('Logs')],
+        floats=[Float(Window(status_bar), right=1, top=0)]
+    ),
+    Window(info_bar),
+    #Window('test'),
+])
 layout = Layout(root_container)
 
-reader, writer = None, None
-use_asyncio_event_loop()
+reader, writer, app = None, None, None
 
 
 def main():
-    app = ptk.Application(layout=layout, key_bindings=kb, full_screen=True)
-    #app.run()
-    asyncio.get_event_loop().run_until_complete(app.run_async().to_asyncio_future())
-    asyncio.run(create_connection())
+    global app
+    loop = asyncio.get_event_loop()
+
+    app = ptk.Application(layout=layout, key_bindings=global_kb, full_screen=True)
+
+    task1 = loop.create_task(create_connection())
+    final_task = asyncio.gather(task1, app.run_async().to_asyncio_future())
+    loop.run_until_complete(final_task)
+    # asyncio.run(create_connection())
 
 
 async def create_connection():
-    global reader, writer
-    reader, writer = await asyncio.open_connection(HOST, PORT)
-    print('Sending message..')
-    await send_data(str(datetime.now()))
-    print('Closing connection.')
-    writer.close()
+    global reader, writer, status_bar
+    try:
+        reader, writer = await asyncio.open_connection(HOST, PORT)
+        addr = writer.get_extra_info('peername')
+        status_bar.text = f'Connected to {addr[0]}:{addr[1]}'
+        app.invalidate()
+
+        data = None
+        while data is not b'':
+            data = await reader.read(200)
+            status_bar.text = f'Received: {data.decode()}'
+            app.invalidate()
+        
+        status_bar.text = 'Server closed the connection.'
+        status_bar.style = style_warning
+        app.invalidate()
+    except OSError as e:
+        status_bar.text = str(e.strerror)
+        status_bar.style = style_warning
+        app.invalidate()
+
+    
+
+    #print('Sending message..')
+    #await send_data(str(datetime.now()))
+    #await asyncio.sleep(15)
+    #print('Closing connection.')
+    #writer.close()
+    #await writer.wait_closed()
+    #print('Waited to close.')
 
 
-async def send_data(data):
+def send_data(data):
     global writer
+    #print('Sending message..')
     writer.write(data.encode())
+    # await writer.drain()
+    #print('Closing connection.')
+    #writer.close()
+    #print(writer.is_closing())
 
 
-@kb.add('q')
+async def wait_out():
+    log_title.text = 'Waiting..'
+    await asyncio.sleep(3)
+    log_title.text = 'OK'
+
+
+@global_kb.add('q')
 def quit(event):
+    global program_quitting
+    program_quitting = True
+    print('Program quitting. Closing connection.')
+    if writer is not None:
+        writer.close()
     event.app.exit()
 
 
-@kb.add('1')
-def _(event):
-    # event.app.reset()
-    global channel_status_labels
-    channel_status_labels[0].text = 'test'
-    global log_title
-    log_title.text = 'Logs 123'
+@global_kb.add('c-i')
+def tab(event):
+    current_control = get_app().layout.current_control
+    get_app().layout.focus_next()
+    info_bar.text = str(type(current_control))
+    if isinstance(current_control, FormattedTextControl):
+        #current_control.text = f'<{current_control.text}>'
+        current_control.style = f'{current_control.style} bold'
+        info_bar.text = current_control.text
+        app.invalidate()
 
+
+@global_kb.add('down')
+def down(event):
+    get_app().layout.focus_next()
+    info_bar.text = str(event)
+
+
+@global_kb.add('up')
+def up(event):
+    get_app().layout.focus_last()
+    info_bar.text = str(event)
 
 if __name__ == '__main__':
     main()
---
+++ controls.py
@@ -0,0 +1,77 @@
+from prompt_toolkit.widgets import Button
+from prompt_toolkit.layout.containers import Window, WindowAlign
+from prompt_toolkit.application import get_app
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
+
+
+class ChannelSwitch(Button):
+
+    instances = []
+
+    def __init__(self, status, handler, handler_args):
+        super().__init__('text', handler)
+        self.status = status
+        self.current_style = None
+        self.control = FormattedTextControl(
+            self._get_text_fragments,
+            key_bindings=self._get_key_bindings(),
+            focusable=True)
+        self.control.show_cursor = False
+        self.handler_args = handler_args
+
+        if status:
+            self.text = 'Enabled'
+            self.current_style = '#5faf00'
+        else:
+            self.text = 'Disabled'
+            self.current_style = '#ff5f5f'
+
+        def get_style():
+            if get_app().layout.has_focus(self):
+                return f'{self.current_style} bold reverse'
+            else:
+                return self.current_style
+
+        self.window = Window(
+            self.control,
+            align=WindowAlign.CENTER,
+            height=1,
+            width=12,
+            style=get_style,
+            dont_extend_width=True,
+            dont_extend_height=True)
+        
+        ChannelSwitch.instances.append(self)
+
+
+    def _get_text_fragments(self):
+        text = ('{:^%s}' % (self.width - 2)).format(self.text)
+
+        def handler(mouse_event):
+            if mouse_event.event_type == MouseEventType.MOUSE_UP:
+                self.handler(self.handleqr_args)
+
+        return [
+            ('class:button.arrow', '<', handler),
+            ('[SetCursorPosition]', ''),
+            ('class:button.text', text, handler),
+            ('class:button.arrow', '>', handler),
+        ]
+
+    def _get_key_bindings(self):
+        " Key bindings for the Button. "
+        kb = KeyBindings()
+
+        @kb.add(' ')
+        @kb.add('enter')
+        def _(event):
+            if self.handler is not None:
+                self.handler(self.handler_args)
+
+        return kb
+        
+        
+        
+    
+    
